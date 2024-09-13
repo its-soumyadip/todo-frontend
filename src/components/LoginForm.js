3c1e57d0fc1cbdb626890d42dde14a5b7e01aea5
Soumyadip Banerjee <bsoumyadip30@gmail.com>
2024-09-14 03:00:41 +0530

    3rd push

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,7 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import '../static/login.css'; // Ensure to link your CSS
 import { login, register, passwordReset } from '../services/api';
 import Loader from './Loader';
-
-
-
 
 const LoginFormDialog = ({ onLogin }) => {
   const dialogRef = useRef(null);
@@ -14,210 +11,199 @@ const LoginFormDialog = ({ onLogin }) => {
   const [newEmail, setNewEmail] = useState('');
   const [newUsername, setNewUsername] = useState('');
   const [newPassword, setNewPassword] = useState('');
-  const [message, setMessage] = useState(''); 
+  const [message, setMessage] = useState('');
   const [loading, setLoading] = useState(false);
   const [modalOpen, setModalOpen] = useState(false);
 
-
-  const openDialog = () => {
+  // Memoized openDialog function
+  const openDialog = useCallback(() => {
     if (!modalOpen) {
-       // Check if the dialog is not already open
-
       dialogRef.current.showModal();
       setModalOpen(true);
       document.body.classList.add('add_blur');
     }
-  };
-  
-  const closeDialog = () => {
-    if (dialogRef.current.open) {  // Check if the dialog is open
+  }, [modalOpen]);
+
+  // Memoized closeDialog function
+  const closeDialog = useCallback(() => {
+    if (dialogRef.current.open) {
       dialogRef.current.close();
       setModalOpen(false);
       document.body.classList.remove('add_blur');
     }
-  };
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    //if login form is selected 
-
     if (selectedForm === 'login') {
       setLoading(true);
-      setMessage('')
-    
-    const success = await login(username, password);
-    if (success) {
-      setLoading(false);
-      onLogin(username);  // Trigger the onLogin function passed from the parent
-      closeDialog();
-    } else {
-      setLoading(false);
-      setMessage('Login failed. Please check your credentials.');
+      setMessage('');
 
-      // if register form is selected 
-
-    }} else if (selectedForm === 'register') {
-      setLoading(true);
-      setMessage('')
-      const success = await register(newUsername, newEmail, newPassword);
+      const success = await login(username, password);
+      setLoading(false);
       if (success) {
-        setLoading(false);
-        onLogin(newUsername);  // Trigger the onLogin function passed from the parent
-        setMessage('Registration successful!'); 
+        onLogin(username);
         closeDialog();
       } else {
-        setLoading(false);
+        setMessage('Login failed. Please check your credentials.');
+      }
+    } else if (selectedForm === 'register') {
+      setLoading(true);
+      setMessage('');
+
+      const success = await register(newUsername, newEmail, newPassword);
+      setLoading(false);
+      if (success) {
+        onLogin(newUsername);
+        setMessage('Registration successful!');
+        closeDialog();
+      } else {
         setMessage('Failed! Make sure your email is not registered.');
       }
-
-      // if password reset form is selected
-
-    } else{
+    } else if (selectedForm === 'password-reset') {
       setMessage('');
-        const success = await passwordReset(newEmail);
-        if (success) {
-          setMessage('Password reset email sent!');
-        } else {
-        
-          setMessage('Failed! Make sure your email is already registered.');
-        }
-      }
-  };
 
+      const success = await passwordReset(newEmail);
+      if (success) {
+        setMessage('Password reset email sent!');
+      } else {
+        setMessage('Failed! Make sure your email is already registered.');
+      }
+    }
+  };
 
   useEffect(() => {
     const token = localStorage.getItem('token');
-    if (!token) { 
-      openDialog(); // Open the dialog automatically if not logged in
+    if (!token) {
+      openDialog();
     }
-  }, []);
+  }, [openDialog]);
 
-    // Watch for changes in `selectForm` and clear the message
-    useEffect(() => {
-      setMessage(''); // Clear message whenever selectForm changes
-    }, [selectedForm]); // Depend on selectForm
+  useEffect(() => {
+    setMessage('');
+  }, [selectedForm]);
 
   return (
     <>
-    <div className='login-register-container'>
-      <button onClick={openDialog} className="login-button">Login | Register</button>
+      <div className='login-register-container'>
+        <button onClick={openDialog} className="login-button">Login | Register</button>
       </div>
       <dialog ref={dialogRef} className="login-dialog login-modal">
-        { loading && <Loader/>}
-
-   
+        {loading && <Loader />}
         <div>
-      {selectedForm === 'login' &&
-        <form method="dialog" onSubmit={handleSubmit} className="login-form">
-          <span className="close-icon" onClick={closeDialog}>&times;</span>
-          <h2>Login</h2>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              className='text-input'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              className='text-input'
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="dialog-buttons">
-            <button type="submit" className="submit-button">Login</button>
-          </div>
-          <div className="login-links">
-            <button onClick={() => setSelectedForm('register')}>Register</button>
-            <button onClick={() => setSelectedForm('password-reset')}>Forgot Password?</button>
-          </div>
-        </form>
-}
+          {selectedForm === 'login' &&
+            <form method="dialog" onSubmit={handleSubmit} className="login-form">
+              <span className="close-icon" onClick={closeDialog}>&times;</span>
+              <h2>Login</h2>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input
+                  type="text"
+                  id="username"
+                  className='text-input'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  value={password}
+                  className='text-input'
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="dialog-buttons">
+                <button type="submit" className="submit-button">Login</button>
+              </div>
+              <div className="login-links">
+                <button onClick={() => setSelectedForm('register')}>Register</button>
+                <button onClick={() => setSelectedForm('password-reset')}>Forgot Password?</button>
+              </div>
+            </form>
+          }
 
-{selectedForm === 'register' &&
- <form method="dialog" onSubmit={handleSubmit} className="login-form" autocomplete="off">
- <span className="close-icon" onClick={closeDialog}>&times;</span>
- <h2>Register</h2>
- <div className="form-group">
-   <label htmlFor="email">Email</label>
-   <input
-     type="email"
-     id="email"
-     className='text-input'
-     onChange={(e) => setNewEmail(e.target.value)}
-     autocomplete="off"
-     required
-   />
- </div>
- <div className="form-group">
-   <label htmlFor="username">Username</label>
-   <input
-     type="text"
-     id="password"
-     onChange={(e) => setNewUsername(e.target.value)}
-    autocomplete="off"
-     className='text-input'
-     required
-   />
- </div>
- <div className="form-group">
-   <label htmlFor="password">Password</label>
-   <input
-     type="password"
-     id="password"
-     className='text-input'
-     onChange={(e) => setNewPassword(e.target.value)}
-    autocomplete="off"
-     required
-   />
- </div>
- <div className="dialog-buttons">
-   <button type="submit" className="submit-button">Register</button>
- </div>
- <div className="login-links">
-   <button onClick={() => setSelectedForm('login')}>Login</button>
-  
- </div>
-</form>
-}
+          {selectedForm === 'register' &&
+            <form method="dialog" onSubmit={handleSubmit} className="login-form" autoComplete="off">
+              <span className="close-icon" onClick={closeDialog}>&times;</span>
+              <h2>Register</h2>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  className='text-input'
+                  value={newEmail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                  autoComplete="off"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="new-username">Username</label>
+                <input
+                  type="text"
+                  id="new-username"
+                  className='text-input'
+                  value={newUsername}
+                  onChange={(e) => setNewUsername(e.target.value)}
+                  autoComplete="off"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="new-password">Password</label>
+                <input
+                  type="password"
+                  id="new-password"
+                  className='text-input'
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  autoComplete="off"
+                  required
+                />
+              </div>
+              <div className="dialog-buttons">
+                <button type="submit" className="submit-button">Register</button>
+              </div>
+              <div className="login-links">
+                <button onClick={() => setSelectedForm('login')}>Login</button>
+              </div>
+            </form>
+          }
 
-{selectedForm === 'password-reset' &&
-  <form method="dialog" onSubmit={handleSubmit} className="pasword-reset-form">
- <span className="close-icon" onClick={closeDialog}>&times;</span>
- <h2>Reset Password</h2>
- <div className="form-group">
-   <label htmlFor="email">Email</label>
-   <input
-     type="email"
-     id="email"
-     className='text-input'
-     onChange={(e) => setNewEmail(e.target.value)}
-     required
-   />
- </div>
- <div className="dialog-buttons">
-   <button type="submit" className="submit-button">Send Email</button>
- </div>
- <div className="login-links">
-   <button onClick={() => setSelectedForm('login')}>Back to Login &#8592;</button>
- </div>
+          {selectedForm === 'password-reset' &&
+            <form method="dialog" onSubmit={handleSubmit} className="password-reset-form">
+              <span className="close-icon" onClick={closeDialog}>&times;</span>
+              <h2>Reset Password</h2>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  className='text-input'
+                  value={newEmail}
+                  onChange={(e) => setNewEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="dialog-buttons">
+                <button type="submit" className="submit-button">Send Email</button>
+              </div>
+              <div className="login-links">
+                <button onClick={() => setSelectedForm('login')}>Back to Login &#8592;</button>
+              </div>
+            </form>
+          }
 
-</form>
-}
-<br></br>
-{message && <p>{message}</p>}
-</div>
-      
+          <br />
+          {message && <p>{message}</p>}
+        </div>
       </dialog>
     </>
   );
